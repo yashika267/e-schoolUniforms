40cb41f6993383380f243e20e46a63f8f3689408
Yashika Goyal <yagoyal@egencia.com>
2018-09-15 13:14:20 +0530

    Initial commit

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,8 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('DB_PORT', '5432');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
